fe4891d8ea53ce090df01384921aa7ca038a045b
Alistair Parkinson <37090819+1011776@users.noreply.github.com>
2018-04-27 00:34:44 +0800

    Create testGame.c
    WH

---
+++ testGame.c
@@ -0,0 +1,15 @@
+// By Alistair Parkinson, username alistairpakinson, zPass z5218710
+// Lucca Harvey, username luccaharveyy, zPass z#######
+// Zac Ching, username zacharyching, zPass z#######
+// CTCH, Cuen Heyning
+// 26/04/2018
+// This code tests mandelbrot.h 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mandelbrot.h"
+    
+int main (int argc, char * argv[]) {
+
+    return EXIT_SUCCESS;
+}
